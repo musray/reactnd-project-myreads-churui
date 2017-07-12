81fcbec28bc67710754ce8876c82d66cf86fc96e
musray <churui.ctec@gmail.com>
2017-07-12 17:24:02 +0800

    refactor ListComponent.js

--- src/ListComponent.js
+++ src/ListComponent.js
@@ -2,44 +2,42 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 import BookShelf from './BookShelf';
 
-class ListComponent extends Component {
-    render() {
-        return (
-            <div className="list-books">
-                <div className="list-books-title">
-                    <h1>MyReads</h1>
-                    {/*{JSON.stringify(this.props.books)}*/}
-                </div>
-                <div className="list-books-content">
-                    <div>
-                        <BookShelf
-                            books={this.props.books}
-                            title="Currently Reading"
-                            readingStatus="reading"
-                            handleChange={this.props.handleChange}
-                        />
-                        <BookShelf
-                            books={this.props.books}
-                            title="Want to Read"
-                            readingStatus="toRead"
-                            handleChange={this.props.handleChange}
-                        />
-                        <BookShelf
-                            books={this.props.books}
-                            title="Read"
-                            readingStatus="read"
-                            handleChange={this.props.handleChange}
-                        />
-                    </div>
-                </div>
-                <div className="open-search">
-                    <Link to="/search" className="open-search">
-                        Add a book
-                    </Link>
-                </div>
-            </div>
-        )
-    }
-}
+const ListComponent = (props) => {
+   return (
+       <div className="list-books">
+           <div className="list-books-title">
+               <h1>MyReads</h1>
+               {/*{JSON.stringify(this.props.books)}*/}
+           </div>
+           <div className="list-books-content">
+               <div>
+                   <BookShelf
+                       books={props.books}
+                       title="Currently Reading"
+                       readingStatus="reading"
+                       handleChange={props.handleChange}
+                   />
+                   <BookShelf
+                       books={props.books}
+                       title="Want to Read"
+                       readingStatus="toRead"
+                       handleChange={props.handleChange}
+                   />
+                   <BookShelf
+                       books={props.books}
+                       title="Read"
+                       readingStatus="read"
+                       handleChange={props.handleChange}
+                   />
+               </div>
+           </div>
+           <div className="open-search">
+               <Link to="/search" className="open-search">
+                   Add a book
+               </Link>
+           </div>
+       </div>
+   )
+};
 
 export default ListComponent;
